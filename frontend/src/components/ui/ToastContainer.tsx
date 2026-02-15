1d6d6758244df141a0e75e3acf533ffab52815ad
sh3ki <shekaigarcia@gmail.com>
2026-02-15 23:23:12 +0800

    Create ToastContainer.tsx

---
+++ frontend/src/components/ui/ToastContainer.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import React from 'react'
+import Toast, { ToastType } from './Toast'
+
+export interface ToastMessage {
+  id: string
+  message: string
+  type: ToastType
+  duration?: number
+}
+
+interface ToastContainerProps {
+  toasts: ToastMessage[]
+  onRemove: (id: string) => void
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ toasts, onRemove }) => {
+  return (
+    <div className="fixed top-4 right-4 z-[10000] flex flex-col gap-3 max-w-md transition-all duration-300">
+      {toasts.map((toast) => (
+        <Toast
+          key={toast.id}
+          message={toast.message}
+          type={toast.type}
+          duration={toast.duration}
+          onClose={() => onRemove(toast.id)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default ToastContainer
